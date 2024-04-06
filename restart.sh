07ed8bff8012914174919d0bf0f50ba798d39112
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-04-06 17:44:21 +0530

    Push pugzarecute's script

--- restart.sh
+++ restart.sh
@@ -4,7 +4,7 @@ set timeout -1
 
 SEGFAULT_TOKEN=$1
 
-spawn ssh -o "SetEnv SECRET=$SEGFAULT_TOKEN" root@lulz.segfault.net
+ spawn ssh -o "SetEnv SECRET=$SEGFAULT_TOKEN" root@lulz.segfault.net
 expect {
     -re "Are you sure you want to continue connecting \\(yes\\/no\\/\\\[fingerprint\\\]\\)\\? " {
         send "yes\n"
@@ -19,9 +19,9 @@ expect {
         exp_continue
     }
     -re ".*└─.*" {
-        send "echo Thanks for hosting OkBuddyGSI Dumper!; "
+        send "echo Restarting segfault!; "
         send "echo Automated Action by PugzAreCute to start OkBuddyGSI dumper bot. For any complaints regarding bugs/abuse contact t.me/pugzarecute, t.me/sounddrill or me@pugzarecute.com; "
-        send "exit \n"
+        send "sudo reboot \n"
         exp_continue
     }
     eof
