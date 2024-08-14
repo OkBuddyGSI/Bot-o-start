68e83a50ef0a8b11683c111fb5429cd9aca05e1d
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-08-14 23:27:45 +0530

    restart.sh: try to exit the ssh

--- restart.sh
+++ restart.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/expect -f
-
 set timeout -1
 set segfaultToken $env(SEGFAULT_TOKEN)
-
 # Check if SEGFAULT_INSTANCE is set and not empty
 if {[info exists env(SEGFAULT_INSTANCE)] && $env(SEGFAULT_INSTANCE) ne ""} {
     # If SEGFAULT_INSTANCE is set and not empty, use its value
@@ -11,7 +9,6 @@ if {[info exists env(SEGFAULT_INSTANCE)] && $env(SEGFAULT_INSTANCE) ne ""} {
     # If SEGFAULT_INSTANCE is not set or empty, set it to the default value
     set SEGFAULT_INSTANCE "lulz.segfault.net"
 }
-
 spawn ssh -o "SetEnv SECRET=$segfaultToken" root@$SEGFAULT_INSTANCE -o StrictHostKeyChecking=accept-new
 expect {
     "root@lulz.segfault.net's password: " {
@@ -26,10 +23,15 @@ expect {
         send "echo Restarting segfault!; "
         send "echo Automated Action by PugzAreCute to start OkBuddyGSI dumper bot. For any complaints regarding bugs/abuse contact t.me/pugzarecute, t.me/sounddrill or me@pugzarecute.com; "
         send "sudo reboot \n"
-        exp_continue
+        # Wait for a short time to allow the reboot command to be sent
+        sleep 2
+        # Close the SSH connection
+        send "exit\r"
+        # Wait for the connection to close
+        expect eof
     }
-    eof
 }
 # credit to PugzAreCute for script
 
+# Exit the script
 exit 0
